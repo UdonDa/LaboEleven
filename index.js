7b2d1382cb9333cc06a9b7c8e5fb91a0b515ae36
Udon <udoooon0727@gmail.com>
2018-04-07 10:22:00 +0900

    [Add]おうむ返し

--- index.js
+++ index.js
@@ -1,15 +1,51 @@
 "use strict";
+require("dotenv").config();
 
-const express = require('express');
-const app = express();
+const server = require("express")();
+const cache = require("memory-cache");
+const path = require("path");
 
-app.set('port', (process.env.PORT || 5000));
-app.use(express.static(__dirname + '/public'));
+//line bot
+const lineBot = require("@line/bpt-sdk");
+const botConfig = {
+	channelAccessToken: process.env.LINE_BOT_ACCESS_TOKEN,
+	channelSecret: process.env.LINE_BOT_CHANNEL_SECRET
+};
+const bot = new lineBot.Client(botConfig);
 
-app.get('/', function(request, response) {
-	response.send('Hello World!')
+server.listen(process.env.PORT || 5000);
+
+server.post("/webhook", lineBot.middleware(botConfig), (req, res, next) => {
+	if (!Array.isArray(req.body.events)) {
+		return res.status(500).end();
+	}
+	res.sendStatus(200);
+
+	Promise
+		.all(req.body.events.map(handleEvent))
+		.then((result) => res.json(result))
+		.catch((err) => {
+			console.error(err);
+			res.status(500).end();
+		});
 });
 
-app.listen(app.get('port'), function() {
-	console.log("Node app is running at localhost:" + app.get('port'))
+// event handler
+function handleEvent(event) {
+	if (event.type !== 'message' || event.message.type !== 'text') {
+		// ignore non-text-message event
+		return Promise.resolve(null);
+	}
+
+	// create a echoing text message
+	const echo = { type: 'text', text: event.message.text };
+
+	// use reply API
+	return client.replyMessage(event.replyToken, echo);
+}
+
+// listen on port
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+	console.log(`listening on ${port}`);
 });
