a161d03aa074facd0e0dd0532347422563590a5b
Udon <udoooon0727@gmail.com>
2018-04-07 10:00:11 +0900

    [Fix]文法

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 const express = require('express');
 const app = express();
 
-app.set('port', (process.env.PORT || 5000))
+app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(request, response) {
